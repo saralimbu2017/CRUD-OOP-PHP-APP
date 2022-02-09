9f1008bab69368bbf5e5203185970d11b94f1e8f
saralimbu2017 <iamsebi7@gmail.com>
2022-02-09 12:40:34 +1100

    Adding database object and setting query

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php include 'header.php' ?>
+<?php 
+  include 'header.php'; 
+  include 'config.php'; 
+  include 'Database.php'; 
 
+  $db = new Database();
+  $query = "SELECT * FROM Student";
+  $data = $db->select($query);
+
+?>
+<table class="mainTable">
+  <tr>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Skill</th>
+    <th>Action</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
 
 
 
