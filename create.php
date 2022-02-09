987d1e76fcb50811135b68c2c20be06a66e8e6b4
saralimbu2017 <iamsebi7@gmail.com>
2022-02-09 14:16:16 +1100

    Adding form

--- create.php
+++ create.php
@@ -2,18 +2,42 @@
   include 'header.php'; 
   include 'config.php'; 
   include 'Database.php'; 
-
  
-
 ?>
-<table class="mainTable">
-  <tr>
+<form action="create.php" method="post">
+  <table class="mainTable">
+    <tr>
+      <td>Name</td>
+      <td>
+        <input type="text" name="name" placeholder="Please enter your name">
+      </td>
+    </tr>
 
-  </tr>
-</table>
+    <tr>
+      <td>Email</td>
+      <td>
+        <input type="text" name="email" placeholder="Please enter your email id">
+      </td>
+    </tr>
+
+    <tr>
+      <td>Skill</td>
+      <td>
+        <input type="text" name="skill" placeholder="Please enter your skill">
+      </td>
+    </tr>
+
+    <tr>
+      <td>Name</td>
+      <td>
+        <input type="submit" name="submit" value="Submit">
+        <input type="reset" value="Cancel">
+      </td>
+    </tr>
+  </table>
+</form>
 
 
-
-
+<a href="index.php"> List Students</a>
 
 <?php include 'footer.php' ?>
