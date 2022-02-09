721bdad179a5e66a11ac17c48dab9e12721b8ca8
saralimbu2017 <iamsebi7@gmail.com>
2022-02-09 14:08:20 +1100

    Create page

---
+++ create.php
@@ -0,0 +1,19 @@
+<?php 
+  include 'header.php'; 
+  include 'config.php'; 
+  include 'Database.php'; 
+
+ 
+
+?>
+<table class="mainTable">
+  <tr>
+
+  </tr>
+</table>
+
+
+
+
+
+<?php include 'footer.php' ?>
